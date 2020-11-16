969f77cd6610f387d84c3cf93f17ea5e239a5239
Roy <roy@interactivelabs.co>
2020-11-16 16:57:12 +0800

    fix: replace intentid & client_secret

--- angular/src/app/we-chat/we-chat.component.ts
+++ angular/src/app/we-chat/we-chat.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { createElement, loadAirwallex } from 'airwallex-payment-elements';
 
-const intentid = 'int_4UQsvc4WzgQ0NIMu5zlocJBeYIg';
-const client_secret =
-  'eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDU1MTYyOTEsImV4cCI6MTYwNTUxOTg5MSwiYWNjb3VudF9pZCI6ImFjNjZjZmRmLWM2ZTgtNGYxYy05NzIzLTYyNzI1NDQyZmExNiIsImRhdGFfY2VudGVyX3JlZ2lvbiI6IlVLIiwiaW50ZW50X2lkIjoiaW50XzRVUXN2YzRXemdRME5JTXU1emxvY0pCZVlJZyJ9.-n78OC6Rk5CRgTX-xOMQ7CHpLYstqBxrj_Cp48YETcE';
+const intentid = 'replace-with-your-intent-id';
+const client_secret = 'replace-with-your-client-secret';
 
 @Component({
   selector: 'app-we-chat',
