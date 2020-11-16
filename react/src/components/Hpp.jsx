2b698b8206060d04f4b3d61a316b12810d468675
Roy <roy@interactivelabs.co>
2020-11-16 14:45:33 +0800

    fix: replace intentid & client_secret

--- react/src/components/Hpp.jsx
+++ react/src/components/Hpp.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { redirectToCheckout, loadAirwallex } from "airwallex-payment-elements";
 
-const intentid = "int_1OF0qMxLz3b8s8unOzIAMjT4qep";
-const client_secret =
-  "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDUyMjk2NzEsImV4cCI6MTYwNTIzMzI3MSwiYWNjb3VudF9pZCI6ImFjNjZjZmRmLWM2ZTgtNGYxYy05NzIzLTYyNzI1NDQyZmExNiIsImRhdGFfY2VudGVyX3JlZ2lvbiI6IlVLIiwiaW50ZW50X2lkIjoiaW50XzFPRjBxTXhMejNiOHM4dW5PeklBTWpUNHFlcCJ9.ltQOAo--yV1aNDjAsnP-TM9m2NMudqR_dHS2OL5lFeA";
+const intentid = "replace-with-your-intent-id";
+const client_secret = "replace-with-your-client-secret";
 
 const Hpp = () => {
   const redirectHpp = async () => {
