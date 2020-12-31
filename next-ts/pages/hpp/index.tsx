d5af733ebf0cba4cdeac265a10c0dc4e881472a1
josie.ku <josie.ku@airwallex.com>
2020-12-31 15:51:29 +0800

    feat: [APAF-1035] payment demo next ts

---
+++ next-ts/pages/hpp/index.tsx
@@ -0,0 +1,29 @@
+import { redirectToCheckout, loadAirwallex } from 'airwallex-payment-elements';
+
+const intent_id = 'replace-with-your-intent-id';
+const client_secret = 'replace-with-your-client-secret';
+
+async function redirectHpp(){
+  try {
+    await loadAirwallex({
+      env: 'staging',
+      origin: window.location.origin
+    });
+    await redirectToCheckout({
+      env: 'staging',
+      id: intent_id,
+      client_secret: client_secret,
+    })
+  } catch(e) {
+    console.error('there was an error', e);
+  }
+}
+
+export default function hpp() {
+  return (
+    <div>
+      <h2>Host Payment Page (HPP) Integration</h2>
+      <button onClick={redirectHpp}>Click to redirect</button>
+    </div>
+  )
+}
---
+++ next-ts/pages/wechat/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import {
+  createElement,
+  loadAirwallex,
+  ElementType
+} from 'airwallex-payment-elements';
+
+const intent_id = 'replace-with-your-intent-id';
+const client_secret = 'replace-with-your-client-secret';
+const ELEMENT_TYPE: ElementType = 'wechat';
+
+export default function wechat() {
+  useEffect(() => {
+    loadAirwallex({
+      env: 'staging',
+    }).then(() => {
+      createElement(ELEMENT_TYPE, {
+        intent: {
+          id: intent_id,
+          client_secret,
+        },
+        origin: window.location.origin,
+      })?.mount(ELEMENT_TYPE);
+    });
+    const onSuccess = (event: CustomEvent) => {
+      /*
+        ... Handle event
+      */
+      console.log(`Confirm success with ${JSON.stringify(event.detail)}`);
+    };
+
+    window.addEventListener('onSuccess', onSuccess as EventListener);
+    return () => {
+      window.removeEventListener('onSuccess', onSuccess as EventListener);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Wechat element integration</h2>
+      <div className="element" id={ELEMENT_TYPE}></div>
+    </div>
+  );
+}
