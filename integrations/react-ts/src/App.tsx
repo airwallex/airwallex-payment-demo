f2cae445f6fa9ea02bd62a96ae9f902e2e4fb95e
Shirly Chen <shirly.chen@airwallex.com>
2023-04-04 13:45:36 +0800

    add exact

--- integrations/react-ts/src/App.tsx
+++ integrations/react-ts/src/App.tsx
@@ -15,16 +15,17 @@ const App: React.FC = () => (
       <div>
         <Suspense fallback={<div />}>
           <Switch>
-            <Route path="/all" component={lazy(() => import('./components/home'))} />
-            <Route path="/hpp" component={lazy(() => import('./components/hpp'))} />
-            <Route path="/drop-in" component={lazy(() => import('./components/dropIn'))} />
-            <Route path="/full-featured-card" component={lazy(() => import('./components/fullFeatureCard'))} />
-            <Route path="/split-card" component={lazy(() => import('./components/splitCard'))} />
-            <Route path="/wechat" component={lazy(() => import('./components/wechat'))} />
-            <Route path="/card" component={lazy(() => import('./components/card'))} />
-            <Route path="/redirect" component={lazy(() => import('./components/redirect'))} />
-            <Route path="/checkout-success" component={lazy(() => import('./components/checkoutSuccess.tsx'))} />
-            <Route path="/instructions" component={lazy(() => import('./components/instructions'))} />
+            <Route path="/" component={lazy(() => import('./components/home'))} />
+            <Route path="/all" exact component={lazy(() => import('./components/home'))} />
+            <Route path="/hpp" exact component={lazy(() => import('./components/hpp'))} />
+            <Route path="/drop-in" exact component={lazy(() => import('./components/dropIn'))} />
+            <Route path="/full-featured-card" exact component={lazy(() => import('./components/fullFeatureCard'))} />
+            <Route path="/split-card" exact component={lazy(() => import('./components/splitCard'))} />
+            <Route path="/wechat" exact component={lazy(() => import('./components/wechat'))} />
+            <Route path="/card" exact component={lazy(() => import('./components/card'))} />
+            <Route path="/redirect" exact component={lazy(() => import('./components/redirect'))} />
+            <Route path="/checkout-success" exact component={lazy(() => import('./components/checkoutSuccess.tsx'))} />
+            <Route path="/instructions" exact component={lazy(() => import('./components/instructions'))} />
           </Switch>
         </Suspense>
       </div>
