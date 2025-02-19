f80eeb6f7e18a017e21841d8e995049b4c8ed59a
Luke Lu <luke.chenyang.lu@airwallex.com>
2025-02-19 10:16:25 +0800

    feat: [CHECKOUT-30] add vue integration

---
+++ integrations/vue-ts/src/components/instructions.vue
@@ -0,0 +1,67 @@
+<!--
+  instructions.vue
+  Airwallex Payment Demo - Vue.  Created by Josie Ku.
+
+  This file is the main instruction page that introduces the
+  demo and provides some trouble-shooting tips.  This is not
+  required for the integration.
+-->
+<template>
+  <div>
+    <h2>Welcome! Here are some tips to help you navigate this project:</h2>
+    <div class="instruction-container">
+      <p id="bullet">1. Feeling confused about these Airwallex functions?</p>
+      <p>
+        Here is the documentation to all our airwallex-payment-elements functions. You will find
+        detailed guides to implementing each payment method with different web frameworks as well as
+        common errors.
+      </p>
+      <a href="https://airwallex.com/docs/js/" target="_blank" rel="noreferrer">
+        Airwallex SDK Documentation
+      </a>
+      <p id="bullet">2. If you run into the following error:</p>
+      <p id="error">Access denied, authentication failed</p>
+      <p>
+        Remember to change the<span id="code">intent_id</span>and
+        <span id="code">client_secret</span>in your component file!
+      </p>
+      <p id="bullet">3. Send us your suggestions!</p>
+      <p>
+        Have some ideas on other ways to integrate airwallex-payment-elements?&nbsp;
+        <a href="https://www.airwallex.com/contact-sales">Let us know!</a> We want to build the best
+        product for you.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Instructions',
+}
+</script>
+<style scoped>
+h2 {
+  text-align: center;
+}
+div.instruction-container {
+  padding: 10px 50px;
+  text-align: left;
+}
+span#code {
+  font-family: monospace;
+  background-color: lightgrey;
+  font-size: 16px;
+  margin: 0px 5px;
+}
+p#bullet {
+  font-weight: bold;
+}
+p#error {
+  background: #ffebee;
+  border-radius: 5px;
+  padding: 5px 10px;
+  max-width: 400px;
+  margin: auto;
+}
+</style>
